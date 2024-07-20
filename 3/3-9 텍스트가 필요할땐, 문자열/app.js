30ab8537b12bda740d9b7e7807f9d6b48dd1578c
SangJin983 <lazistar983@gmail.com>
2024-07-20 18:56:01 +0900

    자바스크립트에서 문자열 사용하는 법

---
+++ 3/3-9 텍스트가 필요할땐, 문자열/app.js
@@ -0,0 +1,8 @@
+const myName = "ken";
+alert(`안녕하세요, 제 이름은 ${myName}입니다. 제 이름은 ${myName.length}글자 입니다.`);
+
+const rightArrow = document.querySelector(".right");
+
+rightArrow.onclick = function () {
+  alert("클릭하셨습니다!");
+};
