2f85bfe2533b83c7f7e327ffa4adfc09fe67806e
SangJin983 <lazistar983@gmail.com>
2024-07-23 05:54:22 +0900

    반복문 배우는 중

---
+++ 4/4-5 전체 메시지 보여주기/app.js
@@ -0,0 +1,19 @@
+/*
+
+for (let i = 5; i < 10; i++) {
+  console.log(`${i}) 물 한컵 떠와.`);
+}
+
+for (let i = 5; i <= 10; i++) {
+  console.log(`${i}) 물 한컵 떠와.`);
+}
+
+for (let i = 5; i === 10; i++) {
+  console.log(`${i}) 물 한컵 떠와.`);
+}
+
+*/
+
+for (let i = 5; i <= 10; i = i + 5) {
+  console.log(`${i}) 물 한컵 떠와.`);
+}
