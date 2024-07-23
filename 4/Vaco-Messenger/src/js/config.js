df7f766cbc2c525eda723075195592f05ca908e4
SangJin983 <lazistar983@gmail.com>
2024-07-23 21:00:06 +0900

    메신저 과제 완성

---
+++ 4/Vaco-Messenger/src/js/config.js
@@ -0,0 +1,10 @@
+// [주의] 붙여넣기 하는 과정에서 export 키워드를 삭제하지 않도록 주의하세요!
+export const firebaseConfig = {
+  apiKey: "AIzaSyAAi_Fwi_P5p2aBQYpDXXUKKHnAfxAGbqg",
+  authDomain: "lazi-messenger.firebaseapp.com",
+  projectId: "lazi-messenger",
+  storageBucket: "lazi-messenger.appspot.com",
+  messagingSenderId: "587298466114",
+  appId: "1:587298466114:web:2469b89b2d9a3c20dd80db",
+  databaseURL: "https://lazi-messenger-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
