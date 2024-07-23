3d3231c13e1e45c64268daeeb32ed1b0a390e658
SangJin983 <lazistar983@gmail.com>
2024-07-23 22:07:48 +0900

    자료형의 종류

---
+++ 5/5-1 자료형의 종류/app.js
@@ -0,0 +1,26 @@
+
+const age = 40;
+const myName = "ken";
+
+const isMyHomeNYC = true;
+const isMyHomeSeoul = false;
+
+const myWallet = null;
+const myLife = undefined;
+
+// 일반 객체
+const object = {
+  age: 40, // object.age 에 들어가는 값
+};
+
+// 배열
+const list = [1, 2, 3];
+
+// 함수
+const doSomething = function () {
+  console.log(40);
+};
+
+doSomething();
+console.log(age);
+console.log(object.age);
