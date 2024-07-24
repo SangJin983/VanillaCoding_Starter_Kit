ce29f7f5c6ffd6f010e27e6270fc8b79543ba045
SangJin983 <lazistar983@gmail.com>
2024-07-24 14:15:22 +0900

    숫자 자료형 연습하기

---
+++ 5/5-2 숫자 자료형/app.js
@@ -0,0 +1,45 @@
+/*
+console.log(30 + 10);
+console.log(30 - 10);
+console.log(30 * 10);
+console.log(30 / 10);
+
+const a = 30;
+const b = 10;
+
+console.log(a + b);
+
+const a = 10;
+const b = 10;
+
+console.log(a + 5 * b);
+
+const a = 10;
+const b = a;
+
+console.log(a + 5 * b);
+
+let age = 40;
+
+age += 2;
+console.log(age);
+
+age++;
+console.log(age);
+
+++age;
+console.log(age);
+
+*/
+
+const a = 10 % 2
+console.log(a);
+
+const wrongInput = undefined // 사용자의 잘못된 입력값이라고 가정
+const result = wrongInput + 10;
+
+console.log(result);
+/* 
+NaN = Not A Number. 숫자 자료형에 들어간 잘못된 값.
+(위치상 숫자값이긴 한데, 숫자가 아니라 오류다)
+*/
