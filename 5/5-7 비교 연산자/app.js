15589d809b979457638ee7effeed3b40a34ad4bf
SangJin983 <lazistar983@gmail.com>
2024-07-24 19:54:22 +0900

    비교 연산자 공부하기

---
+++ 5/5-7 비교 연산자/app.js
@@ -0,0 +1,19 @@
+/*
+const result = 10 > 5;
+console.log(result)
+
+const a = 10 < 100;
+const b = 10 <= 100;
+const c = 100 >= 10;
+
+console.log(a, b, c);
+*/
+
+const a = 0 === false;
+const b = 0 !== false;
+
+const c = 0 == false;
+const d = 0 != false;
+
+console.log(`일치 연산자: ${a}`);
+console.log(`동등 연산자: ${c}`);
