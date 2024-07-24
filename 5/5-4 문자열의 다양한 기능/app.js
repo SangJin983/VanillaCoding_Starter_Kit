f75c8f0301fdc944a4bd04290b664e210f786386
SangJin983 <lazistar983@gmail.com>
2024-07-24 17:24:17 +0900

    문자열의 다양한 기능 공부하기

---
+++ 5/5-4 문자열의 다양한 기능/app.js
@@ -0,0 +1,30 @@
+const a = "ㅋ".repeat(3);
+console.log(a);
+
+const b = "hi ken".includes("ken");
+console.log(b);
+
+const c = "what are you doing?".startsWith("are");
+console.log(c);
+
+const d = "I am doing Fine".endsWith("Fine");
+console.log(d);
+
+const e = "Are you sure?".indexOf(" ");
+console.log(e);
+
+const f = "Yeah I am sure".slice(2,4);
+console.log(f);
+
+const g = "I?doubt?that".split("?");
+console.log(g);
+
+const h = "vaco".split("");
+console.log(h);
+
+const i = "I am Hungry".toLowerCase();
+console.log(i);
+
+const j = "Hello world".toUpperCase();
+console.log(j);
+
