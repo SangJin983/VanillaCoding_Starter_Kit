47323bdac540ba45d0c008af1f7b0e4bc09ac5cc
SangJin983 <lazistar983@gmail.com>
2024-07-24 16:32:25 +0900

    불리언 자료형 연습하기

---
+++ 5/5-3 불리언 자료형/app.js
@@ -0,0 +1,13 @@
+const a = 10;
+const b = 17;
+
+const result = a < b;
+console.log(result);
+
+const s = true;
+const h = false;
+
+console.log(s === h);
+
+const o = new Boolean(true); // Bad
+const t = true; // good
